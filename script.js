e67ef12e274dba261870ddbda2cf6fbf29c84ad8
Sophia Duan <duansophia8@gmail.com>
2025-08-16 11:16:11 -0400

    add form input
    no css yet

---
+++ script.js
@@ -0,0 +1,18 @@
+const scriptURL ="https://script.google.com/macros/s/AKfycbwV_bCSFICxFK0XCfQVi4G6EnlZvtbyVb7-LtSca8dR0r1GuuD9kYq6IhQTDwZAbrQqZg/exec"
+
+const form = document.forms["email-form"];
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    var formData = new FormData(form);
+
+    fetch(scriptURL, {method: "POST", body: formData})
+    .then((response) => {
+        console.log("yaay");
+
+    })
+
+    .catch((error) => {
+        console.log("boooo");
+    });
+})
