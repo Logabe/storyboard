8486c43b81e7cc930b1657df104ee7cddbf7d5a7
Clay Nicholson <82461712+claynicholson@users.noreply.github.com>
2025-08-25 13:08:16 -0400

    -make it jump to fillout form instead -renran

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-const scriptURL ="https://script.google.com/macros/s/AKfycbwV_bCSFICxFK0XCfQVi4G6EnlZvtbyVb7-LtSca8dR0r1GuuD9kYq6IhQTDwZAbrQqZg/exec"
-
 const form = document.forms["email-form"];
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    var formData = new FormData(form);
-    form.reset();
+    const email = document.getElementById("email").value;
 
-    fetch(scriptURL, {method: "POST", body: formData})
-    .then((response) => {
-        console.log("yaay");
-        alert("Emailed address submitted! We'll send you an email shortly");
-    })
+    window.location.href = "https://forms.hackclub.com/story?email=" + encodeURIComponent(email);
+});
 
-    .catch((error) => {
-        console.log("boooo");
-        alert("Looks like your email didn't submit, please try again :)");
-
-    });
-})
